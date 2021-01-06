e8cf58ea0e72b99b227c238952d4b1fbcee71216
holddie <holddie@163.com>
2021-01-06 10:36:10 +0800

    feature: add redis pub/sub scenes。

---
+++ redis-lock/src/main/java/com/holddie/redis/event/PubSubMain.java
@@ -0,0 +1,41 @@
+package com.holddie.redis.event;
+
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Slf4j
+public class PubSubMain {
+
+	public static final String CHANNEL_NAME = "MyChannel";
+	public static final String REDIS_HOST = "127.0.0.1";
+	public static final int REDIS_PORT = 6379;
+
+	private final static JedisPoolConfig POOL_CONFIG = new JedisPoolConfig();
+	private final static JedisPool JEDIS_POOL =
+			new JedisPool(POOL_CONFIG, REDIS_HOST, REDIS_PORT, 0);
+
+	public static void main(String[] args) {
+		final Jedis subscriberJedis = JEDIS_POOL.getResource();
+		final Jedis publisherJedis = JEDIS_POOL.getResource();
+		final Subscriber subscriber = new Subscriber();
+		//订阅线程：接收消息
+		new Thread(() -> {
+			try {
+				log.info("Subscribing to \"MyChannel\". This thread will be blocked.");
+				subscriberJedis.subscribe(subscriber, CHANNEL_NAME);
+				log.info("Subscription ended.");
+			}
+			catch (Exception e) {
+				log.error("Subscribing failed.", e);
+			}
+		}).start();
+
+		new Publisher(publisherJedis, CHANNEL_NAME).startPublish();
+		publisherJedis.close();
+
+		subscriber.unsubscribe();
+		subscriberJedis.close();
+	}
+}
